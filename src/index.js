12665c3f51acc1e90ada0d3bb45bb869ef731432
Michelle <nyamburamichelle400@gmail.com>
2024-04-07 16:11:00 +0300

    completes assignment

--- src/index.js
+++ src/index.js
@@ -1,83 +1,132 @@
-document.addEventListener('DOMContentLoaded', () => {
+   // Function to add data to the DOM
+    //function addInfo(data) {
+      Promise.all([
 
-    // Declare variables for easy reference
-    const baseUrl = "http://localhost:3000";
-    
-    
-    // Function to add data to the DOM
-    function addInfo(data) {
-        fetch(`${baseUrl}`)
+        fetch("http://localhost:3000/images"),
+        fetch("http://localhost:3000/comments")
+      ]).then(function(responses) {
+        return Promise.all(responses.map(function(response) {
+          return response.json()
         
+      }))})
         .then(data => {
-            
-        // Select main area
-            const form = document.querySelector('.image-container');
-            
-            // UNDER IMAGES
-            // Place the image
-            const image = document.querySelector('.logo');
-            const myImage = document.createElement('img');
-            myImage.src = `${data.image}`;
-            image.appendChild(myImage);
-            
-            // Add the number of likes
-                const numLikes = document.querySelector("#like-count");
-                const myNumLikes = document.createElement('span');
-                myNumLikes.textContent =`${ data.numLikes}`;
-                numLikes.appendChild(myNumLikes);
-                
-                // Add the title
-                const title = document.querySelector("#card-title");
-                const myTitle = document.createElement("h2");
-                myTitle.textContent = `${data.title}`;
-                title.appendChild(myTitle);
-                
-                // UNDER COMMENTS
-                // Add the comments
-                const comments = document.querySelector('#comments-list');
-                const myComments = document.createElement("li");
-                myComments.textContent = `${data.content}`;
-                comments.appendChild(myComments);
-                
-                // Append elements to the form
-                form.appendChild(image);
-                form.appendChild(numLikes);
-                form.appendChild(title);
-                form.appendChild(comments);
-                
+          //console.log(data);   //it works!
+
+
+
+
+         const myTitle=document.querySelector("#card-title");
+         //console.log(myTitle);
+          myTitle.innerHTML = data[0][0].title
+
+         //place the image
+         const myImage=document.querySelector("#card-image");
+         //console.log(myImage);
+          myImage.src=data[0][0].image
+
+          //Place the comments 
+          //iterate over the comments
+          const myComments=document.querySelector("#comments-list");
+          if(data[1].length >0) {
+            myComments.innerHTML = '' //clear the existing comments
+            data[1].forEach(comment => {
+              myComments.innerHTML += `<p>${comment.content}</p>`;
             })
+          }else{
+            console.log("Comments cannot be displayed")
+          }
+
+         const myLikes=document.querySelector("#like-count");
+         myLikes.innerHTML=`${data[0][0].likes} likes`
+
+          }
+        )
+
+        .catch(function(err) {console.log("error occurred!")})
+     // }
+
+
+
+
+
+     
+
+// updating the number of likes
+
+const likebtn = document.querySelector("#like-button")
+  //console.log(likebtn)
+  likebtn.addEventListener("click",function(e) {
+ 
+
+  })
+
+ // referenes
+const likeButton = document.getElementById("like-button");
+const myLikes=document.querySelector("#like-count");
+
+// Initially set likes count to 0
+let likes = 0;
+
+// Function to update the likes count display
+function updateLikesDisplay() {
+  myLikes.textContent = likes + " likes";
 }
 
+// Add event listener to the like button
+likeButton.addEventListener("click", function() {
+  // Increment the likes count
+  likes++;
+  // Update the likes count display
+  updateLikesDisplay();
+});
 
-const button=document.querySelector('#like-button');
-button.addEventListener('click', function (event) {
-    if (event.target.matches('#like-button')) {
-      const imageId = event.target.id;
-      const currentLikesElement = event.target.previousSibling;
-      const currentLikes = parseInt(currentLikesElement.textContent.split(' ')[0]); // Extract current likes count from DOM
-      const newLikes = currentLikes + 1;
 
-      updateLikesCount(imageId, newLikes)
-        .then(updatedLikes => {
-          currentLikesElement.textContent = `${updatedLikes.likes} Likes`;
-        })
-        .catch(error => {
-          console.error('Error updating likes count:', error);
-        });
-    }
-  });
 
-  function updateLikesCount(imagesId, newLikes) {
-    return fetch('http://localhost:3000', {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify({
-        likes: newLikes
-      })
+
+
+
+
+// POSTS 
+const commentbtn=document.querySelector(".comment-button");
+//console.log(commentbtn)
+
+const commentForm = document.getElementById("comment-form");//is a form in the HTML
+
+  const commentInput = document.getElementById("comment");// input in the HTML
+
+  const myComments=document.querySelector("#comments-list");// the list item in the HTML
+
+
+
+  commentForm.addEventListener("submit",function(e) {
+     e.preventDefault();// prevernts default reload
+
+     const commentTexts= commentInput.value
+     //console.log(commentTexts)
+     const commentListItem = document.createElement("li");
+     //create a list for the new comments
+     commentListItem.textContent = commentTexts;
+
+     // Append the new comment list item to the comments list
+     myComments.appendChild(commentListItem);
+
+     commentForm.reset()
+
+     function addComment
+     (commentText) {
+       // Create a new comment list item
+       const commentListItem = document.createElement("li");
+       commentListItem.textContent = commentText;
+       
+       // Append the new comment list item to the comments list
+       myComments.appendChild(commentListItem);
+      }
+      
+      addComment()
+      
     })
-    .then(response => response.json());
-  }
-})
+
+
+
+
+
